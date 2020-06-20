19182fad17edd1c8e057b23c77b728c2d16e2ae5
jasonxjung <jasonjung.xc@gmail.com>
2020-06-20 09:54:46 +0300

    get all posts route added

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -5,10 +5,10 @@ const { check, validationResult } = require('express-validator');
 const Post = require('../../models/Post');
 const Profile = require('../../models/Profile');
 const User = require('../../models/User');
-// @router POST api/posts
-// @desc Test route
-// @access Private
 
+// @router POST api/posts
+// @desc Create a post
+// @access Private
 router.post(
   '/',
   [auth, [check('text', 'Text is required').not().isEmpty()]],
@@ -35,4 +35,29 @@ router.post(
   }
 );
 
+// @router GET api/posts
+// @desc Get all posts
+// @access Private
+router.get('/', auth, async (req, res) => {
+  try {
+    const posts = await Post.find().sort({ date: -1 }); //fetch posts and return the latest post first.
+    res.json(posts);
+  } catch (err) {
+    console.error(err.message);
+    return res.status(500).send('Server Error');
+  }
+});
+
+// @router GET api/posts
+// @desc Get all posts
+// @access Private
+router.get('/', auth, async (req, res) => {
+  try {
+    const posts = await Post.find().sort({ date: -1 }); //fetch posts and return the latest post first.
+    res.json(posts);
+  } catch (err) {
+    console.error(err.message);
+    return res.status(500).send('Server Error');
+  }
+});
 module.exports = router;
